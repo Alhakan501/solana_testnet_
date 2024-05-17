6deb625657f2a5a1e83e0ee50cca4f83a3caa5b8
Alhakan501 <alhakansol@gmail.com>
2024-05-17 14:43:11 +0000

    Update solana_script1.py

--- solana_script1.py
+++ solana_script1.py
@@ -1,8 +1,10 @@
 #mport the necessary libraries
+import sys
 from solana.rpc.api import Client
 from solders.keypair import Keypair
 from solana.exceptions import *
 import info as stored
+import time
 
 
 #create a class 
@@ -14,7 +16,7 @@ class test_net:
         '''
         self.public_key=''
         self.testnet_rpc="https://api.testnet.solana.com" #testnet rpc url
-
+        self.LAMPORT_AMOUNT=1000000000                                                   #lamport amount
 
 
     def generate_key(self):
@@ -26,6 +28,8 @@ class test_net:
         with open("info.py","w") as f:
             f.write(f'keys={data}')
        
+
+
 
 
 
@@ -42,27 +46,37 @@ class test_net:
     
     
     
+    
+    
 
     def sol_request(self,data):
         '''
         method that requests sol for the user from the testnet rpc
         '''
-        
         self.connect_to_rpc(data)                                                          #call the connect_to_rpc function and pass the data as args
         print('your public key is : ',self.public_key)
         self.public_key=self.public_key
-        amount=int(input("Enter amount  you want to request: "))                           # prompt theuser to enter the amount of sol needed
+        print(self.public_key)
+        amount=int(input("Enter lamport amount  you want to request: "))                           # prompt theuser to enter the amount of sol needed
         if amount<=0:                                                                       #check if the amount is positive
             print("Enter a positive amount ")
             self.sol_request(data)
         else:
             try:                                                                           #try to request the sol from the  testnet rpc
-                request_sol=self.client.request_airdrop(self.public_key,amount)
+                solamount=float(amount/self.LAMPORT_AMOUNT )                                               #convert the lamport to amount
+                print(f'you are requesting {solamount} SOL.....')
+                self.request_sol=self.client.request_airdrop(self.public_key,amount,)
                 print('amount has been airdroped successfully.......')
+                # conf=self.client.confirm_transaction(self.request_sol.value)
+                time.sleep(20)
+                balance=self.client.get_balance(self.public_key)
+                print(f'your current balance is {balance.value/self.LAMPORT_AMOUNT} SOL' )
                 
             except SolanaRpcException as e:                                                #exception handler
                 print('Error: too many requests has been made \nTry again after some time',e)
+                sys.exit(1)
             print(f'Please check your balance' )
+            
         
         
         
@@ -73,12 +87,21 @@ class test_net:
         this method is responsible for getting the balance of the user from the testnet rpc
         '''
         self.connect_to_rpc(data)
+        time.sleep(15)
         balance=self.client.get_balance(self.public_key)
-        print(f'your current balance is {balance.value} SOL' )
+        print(f'your current balance is  {balance.value/self.LAMPORT_AMOUNT}SOL' )
+        
+        
+        
+        
+        
+        
         
 
 
 if __name__=="__main__":                                                                   #run the program
         
     test=test_net()
-    test.sol_request(stored.keys)
+    # test.generate_key()
+    # test.sol_request(stored.keys)
+    test.get_balance(stored.keys)
